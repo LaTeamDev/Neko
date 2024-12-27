ae7a3ae2f38bc94f96293a4fe4142c033ba443dd
Влад Кот <vanderofcat@gmail.com>
2024-12-28 00:58:28 +0300

    implemented basic dialogue system

--- NeuroSama/GameScene.cs
+++ NeuroSama/GameScene.cs
@@ -1,5 +1,6 @@
 ﻿using NekoLib.Core;
 using NekoRay;
+using NeuroSama.Gameplay.Dialogue;
 using ZeroElectric.Vinculum;
 using Camera2D = NekoRay.Camera2D;
 
@@ -10,6 +11,8 @@ public class GameScene : BaseScene {
         var gameObject = new GameObject("Camera");
         var camera = gameObject.AddComponent<Camera2D>();
         camera.IsMain = true;
+
+        gameObject.AddChild("Dialogue Box").AddComponent<DialogueDrawerComponent>();
 
         base.Initialize();
     }
---
+++ NeuroSama/Gameplay/Dialogue/DialogueController.cs
@@ -0,0 +1,18 @@
+﻿using NekoRay.Tools;
+
+namespace NeuroSama.Gameplay.Dialogue;
+
+public static class DialogueController {
+    public static Queue<DialogueEntry> Queue = new();
+    public static bool HasMessages => Queue.Count != 0;
+
+    [ConCommand("dg_add")]
+    public static void Add(string name, string text) {
+        Queue.Enqueue(new DialogueEntry(name, text));
+    }
+    
+    [ConCommand("dg_next")]
+    public static DialogueEntry Next() {
+        return Queue.Dequeue();
+    }
+}
