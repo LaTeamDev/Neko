c2ffd6d2416d199573854c80b1097f79f3b149ee
Влад Кот <vanderofcat@gmail.com>
2024-12-27 23:02:48 +0300

    neurosama jam initial commit

---
+++ NeuroSama/Game.cs
@@ -0,0 +1,11 @@
+﻿using NekoLib.Scenes;
+using NekoRay;
+
+namespace NeuroSama;
+
+public class Game : GameBase {
+    public override void Load(string[] args) {
+        base.Load(args);
+        SceneManager.LoadScene(new GameScene());
+    }
+}
---
+++ NeuroSama/GameScene.cs
@@ -0,0 +1,16 @@
+﻿using NekoLib.Core;
+using NekoRay;
+using ZeroElectric.Vinculum;
+using Camera2D = NekoRay.Camera2D;
+
+namespace NeuroSama;
+
+public class GameScene : BaseScene {
+    public override void Initialize() {
+        var gameObject = new GameObject("Camera");
+        var camera = gameObject.AddComponent<Camera2D>();
+        camera.IsMain = true;
+
+        base.Initialize();
+    }
+}
