bb16d1076f2663a7491a55d79be7d0d2bb86f3f6
Влад Кот <vanderofcat@gmail.com>
2024-12-29 21:46:58 +0300

    implemented preloader of assets

--- NeuroSama/Game.cs
+++ NeuroSama/Game.cs
@@ -8,6 +8,7 @@ namespace NeuroSama;
 public class Game : GameBase {
     public override void Load(string[] args) {
         base.Load(args);
+        Preloader.Preload();
         if (DevMode) {
             SceneManager.LoadScene(new GameScene());
         }
